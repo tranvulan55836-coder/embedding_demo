8788e968ed407f2e54932eb6103b1b37b47023dd
BB <tranvulan55836@gmail.com>
2026-05-28 19:53:44 +0800

    Text embedding demo

--- embedding.py
+++ embedding.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
 def get_embedding(text: str, model: str = "text-embedding-3-small"):
     """
     调用 OpenAI API 获取文本 embedding
